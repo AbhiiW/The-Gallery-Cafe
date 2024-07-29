e9ed7fef2f16a7ccfeb1814338ef514deed6b6c8
Tharindu Darshana <94457266+tdbpathiraja@users.noreply.github.com>
2024-07-29 21:22:56 +0530

    path edited

--- assets/php/Admin_Functions/create_staff.php
+++ assets/php/Admin_Functions/create_staff.php
@@ -26,7 +26,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     $stmt->close();
     $conn->close();
-    header("Location: ../../Pages/admindashboard.php");
+    header("Location: ../../../Pages/Dashboards/admindashboard.php");
     exit();
 }
 ?>
